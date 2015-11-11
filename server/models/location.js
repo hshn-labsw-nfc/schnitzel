f7b6f12badf6472749c875457837edd1cc9506ed
theshark <mweigel@beamng.com>
2015-11-11 23:28:23 +0100

    added tagid to locationmodel

--- server/models/location.js
+++ server/models/location.js
@@ -3,7 +3,8 @@ var mongoose = require('mongoose');
 var LocationSchema   = new mongoose.Schema({
     room: String,
     name: String,
-    description: String
+    description: String,
+    tagID: String
 });
 
 module.exports = mongoose.model('Location', LocationSchema);
