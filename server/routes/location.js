05be1958037e716686a7c57311ac41e2be5eaf2b
therealshark <miweigel@stud.hs-heibronn.de>
2015-11-11 06:58:44 +0100

    removed timeout

--- server/routes/location.js
+++ server/routes/location.js
@@ -1,24 +1,24 @@
 var express = require('express');
 var router = express.Router();
 
+var data = [
+    {
+        _id: 0,
+        room: 'A105',
+        name: 'Labor',
+        description: 'das ist ne beschreibung'
+    },
+    {
+        _id: 1,
+        room: 'A106',
+        name: 'Labor',
+        description: 'das ist ne andere beschreibung'
+    }
+];
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-    res.send(
-        [
-            {
-                _id: 0,
-                room: 'A105',
-                name: 'Labor',
-                description: 'das ist ne beschreibung'
-            },
-            {
-                _id: 1,
-                room: 'A106',
-                name: 'Labor',
-                description: 'das ist ne andere beschreibung'
-            }
-        ]
-    );
+    res.send(data);
 });
 
 module.exports = router;
