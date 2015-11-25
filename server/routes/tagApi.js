7ecc631413afdb10718bc42d1f62160abe53ac8c
theshark <mweigel@beamng.com>
2015-11-25 15:28:40 +0100

    improved frontend api, added basic scribble of api functionality

--- server/routes/tagApi.js
+++ server/routes/tagApi.js
@@ -6,20 +6,34 @@ var Riddle = require('../models/riddle');
 var Tag = require('../models/tag');
 var PlaySession = require('../models/playSession');
 
-router.post('/start', startSchnitzel);
+router.post('/start', startPlaySession);
 router.get('/state', getState);
 router.post('/riddle', solveRiddle);
+router.get('location/:id', getLocation)
 
-function startSchnitzel(req, res, next) {
+// Will return the sessionid of the playsession
+function startPlaySession(req, res, next) {
     res.send('This will be the route to start a schnitzeljagt');
 }
 
+// Will return the current state
+//State: {
+//    riddleToSolve: Does the nextRiddle to be solved before progress can be made?,
+//    nextRiddle: {riddle: Question to solve, (... more stuff later: different riddletypes, etc.)},
+//    nextLocation: Location-Description (?)
+//}
 function getState(req, res, next) {
     res.send('This will be the route to get the current state (next location, next riddle, progress)');
 }
 
+// Will return whether the sent solution was right
 function solveRiddle(req, res, next) {
     res.send('This will be the route to send a riddlesolution to the server');
+}
+
+// Will return the location based on a tagid
+function getLocation(req, res, next){
+
 }
 
 module.exports = router;
