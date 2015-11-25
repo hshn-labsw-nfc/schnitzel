4d85822fd841f50f5882761634083bf69895f99c
theshark <mweigel@beamng.com>
2015-11-26 00:20:16 +0100

    worky work work

--- client/tag/js/controllers/tag.js
+++ client/tag/js/controllers/tag.js
@@ -3,15 +3,19 @@
 
     app.controller('TagCtrl', TagCtrl);
 
-    function TagCtrl($scope, $routeParams, $http) {
+    function TagCtrl($scope, $routeParams, $http, $rootScope) {
         var tagID = $routeParams.tagID;
 
         if($scope.game.running){
             console.log('/api/game/location/' + $scope.game.sessionID);
             $http.post('/api/game/location/' + $scope.game.sessionID, {tagID: tagID}).then(function(res){
                 if(res.status == 200){
-                    console.log(res.data.correctLocation);
                     $scope.correctLocation = res.data.correctLocation;
+                    if($scope.correctLocation){
+                        $rootScope.$broadcast('fetchState');
+                    }else{
+                        location.hash = '/';
+                    }
                 }else{
 
                 }
