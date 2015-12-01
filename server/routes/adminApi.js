6f26ecb7a174d626823ab0f15caea00364a29b65
theshark <mweigel@beamng.com>
2015-12-01 10:04:09 +0100

    added login structure

--- server/routes/adminApi.js
+++ server/routes/adminApi.js
@@ -10,4 +10,21 @@ router.use('/locations', RestFactory(Location));
 router.use('/riddles', RestFactory(Riddle));
 router.use('/tags', RestFactory(Tag));
 
+router.post('/session', function(req, res, next){
+    var user = req.body.user;
+    var password = req.body.password;
+    if(!user || !password) {
+        res.status(401)
+    }
+    if(user == 'test' && password == 'pw'){
+        res.send({
+            token: 'meintoken'
+        });
+    }
+});
+
+router.delete('/session/:id', function(req, res, next){
+
+});
+
 module.exports = router;
