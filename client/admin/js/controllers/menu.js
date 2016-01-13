9aa07ea6001048077675468546433eabd290674a
theshark <mweigel@beamng.com>
2016-01-13 20:59:27 +0100

    Rechtschreibung und co.

--- client/admin/js/controllers/menu.js
+++ client/admin/js/controllers/menu.js
@@ -7,7 +7,7 @@
         $scope.showmenu = auth.isLoggedIn();
         $scope.items = {
             status: {name:'Status', icon: 'glyphicon glyphicon-eye-open'},
-            config: {name:'Config', icon: 'glyphicon glyphicon-cog'},
+            config: {name:'Konfiguration', icon: 'glyphicon glyphicon-cog'},
             location: {name:'Orte', icon: 'glyphicon glyphicon-pushpin'},
             riddle: {name:'Rätsel', icon: 'glyphicon glyphicon-text-size'},
             tag: {name:'Tags', icon: 'glyphicon glyphicon-tags'}
