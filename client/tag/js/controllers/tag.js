4d3e633e94667861f59bd69ec027d0980c0605e3
theshark <mweigel@beamng.com>
2016-03-10 01:33:35 +0100

    added usabilityimprovement on adminmode when scanning tag

--- client/tag/js/controllers/tag.js
+++ client/tag/js/controllers/tag.js
@@ -6,22 +6,24 @@
     function TagCtrl($scope, $routeParams, $http, $rootScope) {
         var tagID = $routeParams.tagID;
 
-        if($scope.game.running){
+        if($scope.game.running) {
             console.log($scope.game);
-            $http.post('/api/game/sessions/' + $scope.game.sessionID + '/location', {tagID: tagID}).then(function(res){
-                if(res.status == 200){// {correctLocation: true}
-                    if(res.data.correctLocation){
+            $http.post('/api/game/sessions/' + $scope.game.sessionID + '/location', {tagID: tagID}).then(function (res) {
+                if (res.status == 200) {// {correctLocation: true}
+                    if (res.data.correctLocation) {
                         $rootScope.$broadcast('alert', 'Raum gefunden', 'success');
-                    }else{
+                    } else {
                         $rootScope.$broadcast('alert', 'Das ist der falsche Raum!', 'danger');
                     }
                     $rootScope.$broadcast('fetchState');
-                }else{
+                } else {
                     $rootScope.$broadcast('alert', 'The server didn\'t respond, Please reload the page to try again', 'danger');
                 }
             });
+        }else if(localStorage.hasOwnProperty('admintoken')){
+            $rootScope.$broadcast('alert', 'Tag "' + tagID + '" gescannt', 'warning');
+            setTimeout(function(){location.href = '/admin/#/listtags';}, 2000);
         }else{
-            $rootScope.$broadcast('alert', 'Session doesn\'t exist', 'danger');
             location.hash = '/';
         }
     }
