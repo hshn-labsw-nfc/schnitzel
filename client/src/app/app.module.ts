fc490656c5540fc5d093f33c3a42f499f4a3368d
Dominic <dlehrmos@stud.hs-heilbronn.de>
2017-10-19 16:02:34 +0200

    added basic user/admin structure

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -2,13 +2,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AdminComponent } from './admin/admin.component';
+import { UserComponent } from './user/user.component';
+import {RouterModule, Routes} from '@angular/router';
+import {HttpModule} from '@angular/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AdminComponent,
+    UserComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpModule,
+    RouterModule.forRoot([
+      {path: 'admin', component: AdminComponent},
+      {path: '**', component: UserComponent}
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
