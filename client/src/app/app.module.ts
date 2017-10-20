45119b6b1319b6370b267bce53ccfcc19793b745
Dominic Lehrmoser <dlehrmos@stud.hs-heilbronn.de>
2017-10-20 18:33:43 +0200

    added basic routing for the client

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -7,6 +7,76 @@ import { UserComponent } from './user/user.component';
 import {RouterModule, Routes} from '@angular/router';
 import {HttpModule} from '@angular/http';
 
+const routes: Routes = [
+  {path: 'admin', component: AdminComponent,
+    children: [
+        {
+          path: 'listriddles',
+          component: AdminComponent
+        },
+        {
+          path: 'addriddle',
+          component: AdminComponent
+        },
+        {
+          path: 'edittiddle:id',
+          component: AdminComponent
+        },
+        {
+          path: 'listlocations',
+          component: AdminComponent
+        },
+        {
+          path: 'addlocation',
+          component: AdminComponent
+        },
+        {
+          path: 'editlocation:id',
+          component: AdminComponent
+        },
+        {
+          path: 'listtags',
+          component: AdminComponent
+        },
+        {
+          path: 'addtag',
+          component: AdminComponent
+        },
+        {
+          path: 'edittag:id',
+          component: AdminComponent
+        },
+        {
+          path: 'login',
+          component: AdminComponent
+        },
+        {
+          path: 'liststatuss',
+          component: AdminComponent
+        },
+        {
+          path: '**',
+          redirectTo: '/admin/login'
+        }
+      ]
+  },
+  {path: 'user', component: UserComponent, children: [
+    {
+      path: ':tagID',
+      component: UserComponent
+    },
+    {
+      path: '',
+      component: UserComponent
+    },
+    {
+      path: '**',
+      redirectTo: '/user'
+    }
+  ]},
+  {path: '**', redirectTo: 'user'}
+]
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -16,10 +86,7 @@ import {HttpModule} from '@angular/http';
   imports: [
     BrowserModule,
     HttpModule,
-    RouterModule.forRoot([
-      {path: 'admin', component: AdminComponent},
-      {path: '**', component: UserComponent}
-    ])
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
