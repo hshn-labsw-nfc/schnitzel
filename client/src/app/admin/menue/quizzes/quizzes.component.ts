b4d3b76f16390ed6b03c36fa89110d290b77f38a
Dominic Lehrmoser <dlehrmos@stud.hs-heilbronn.de>
2017-10-24 19:41:38 +0200

    implemented admin quiz list

--- client/src/app/admin/menue/quizzes/quizzes.component.ts
+++ client/src/app/admin/menue/quizzes/quizzes.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+import {HttpClient, HttpHeaders} from '@angular/common/http'//;
 
 @Component({
   selector: 'app-admin-quizzes',
@@ -7,9 +8,69 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdminQuizzesComponent implements OnInit {
 
-  constructor() { }
+  @Input() adminToken: string;
 
-  ngOnInit() {
+  constructor(private http: HttpClient) {
+    this.tableHeaders.push('Rätselname');
+    this.tableHeaders.push('Beschreibung');
+    this.tableHeaders.push('ID');
   }
 
+  public quizzes = new Array<AdminQuiz>();
+  public tableHeaders = new Array<string>();
+
+  ngOnInit() {
+    this.loadQuizzesFromServer();
+  }
+
+  loadQuizzesFromServer() {
+    console.log('loading current quizzes from server');
+    this.http.get('/api/admin/riddles',{headers: new HttpHeaders().set('X-Auth-Token', this.adminToken)}).subscribe(
+      (data) => {
+        this.quizzes = new Array<AdminQuiz>()
+        console.log('loaded current quizzes',data);
+        for(let d in data){
+           this.quizzes.push(
+            new AdminQuiz(data[d]['answer'],
+              data[d]['description'],
+              data[d]['hint'],
+              data[d]['image'],
+              data[d]['name'],
+              data[d]['_id']));
+        }
+        console.log('initialized array',this.quizzes);
+      },
+      (err) => {
+        console.log('loaded current quizzes error', err);
+      }
+    );
+  }
+  addQuiz() {
+
+  }
+  editQuiz(id: string) {
+
+  }
+  deleteQuiz(id: string) {
+    console.log('delete quiz',id);
+    this.http.delete('/api/admin/riddles/'+id,{headers: new HttpHeaders().set('X-Auth-Token', this.adminToken)}).subscribe(
+      (data) => {
+        console.log('successfully deleted quiz', id);
+        this.loadQuizzesFromServer();
+      },
+      (err) => {
+        console.log('error deleting location', err);
+      }
+    );
+  }
+}
+
+export class AdminQuiz {
+  constructor(public answer: string,
+              public description: string,
+              public hint: string,
+              public image: any,
+              public name: string,
+              public _id: string) {
+  }
 }
