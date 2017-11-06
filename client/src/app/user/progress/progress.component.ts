6db0fa52b63cf06398ec4ff4c73b936ffc8f27ca
Dominic Lehrmoser <dlehrmos@stud.hs-heilbronn.de>
2017-11-06 22:08:27 +0100

    added delete session button on user side

--- client/src/app/user/progress/progress.component.ts
+++ client/src/app/user/progress/progress.component.ts
@@ -1,4 +1,6 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+import {MatDialog} from '@angular/material';
+import {SharedSimpleDialogComponent} from '../../shared/simple-dialog/simple-dialog.component';
 
 @Component({
   selector: 'app-user-progress',
@@ -9,10 +11,27 @@ export class UserProgressComponent implements OnInit {
 
   @Input() progressCount: number;
   @Input() progressDone: number;
+  @Output()
+  progressOutput: EventEmitter<string> = new EventEmitter();
 
-  constructor() {
+  constructor(private dialog: MatDialog) {
   }
 
   ngOnInit() {
+  }
+
+  abbrechen() {
+    const deleteSession = this.dialog.open(SharedSimpleDialogComponent, {data: {
+      title: 'Schnitzeljagd löschen',
+      message: 'Möchtest du deine aktuelle Schnitzeljagd wirklich (permanent) löschen?',
+      button1: 'JA LÖSCHEN',
+      button2: 'Abbrechen'
+    }});
+    deleteSession.afterClosed().subscribe(result => {
+      if(result === 'b1') {
+        console.log('user deleted session');
+        this.progressOutput.emit();
+      }
+    });
   }
 }
