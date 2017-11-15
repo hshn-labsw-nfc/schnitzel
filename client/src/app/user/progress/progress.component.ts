70bf7883225fe9762a0bc100a377ced6a43e17ab
Dominic Lehrmoser <dlehrmos@stud.hs-heilbronn.de>
2017-11-15 19:27:18 +0100

    removed old button

--- client/src/app/user/progress/progress.component.ts
+++ client/src/app/user/progress/progress.component.ts
@@ -1,6 +1,4 @@
-import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
-import {MatDialog} from '@angular/material';
-import {SharedSimpleDialogComponent} from '../../shared/simple-dialog/simple-dialog.component';
+import {Component, Input, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-user-progress',
@@ -11,27 +9,10 @@ export class UserProgressComponent implements OnInit {
 
   @Input() progressCount: number;
   @Input() progressDone: number;
-  @Output()
-  progressOutput: EventEmitter<string> = new EventEmitter();
 
-  constructor(private dialog: MatDialog) {
+  constructor() {
   }
 
   ngOnInit() {
-  }
-
-  abbrechen() {
-    const deleteSession = this.dialog.open(SharedSimpleDialogComponent, {data: {
-      title: 'Schnitzeljagd löschen',
-      message: 'Möchtest du deine aktuelle Schnitzeljagd wirklich (permanent) löschen?',
-      button1: 'JA LÖSCHEN',
-      button2: 'Abbrechen'
-    }});
-    deleteSession.afterClosed().subscribe(result => {
-      if(result === 'b1') {
-        console.log('user deleted session');
-        this.progressOutput.emit();
-      }
-    });
   }
 }
