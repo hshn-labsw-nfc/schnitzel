1277c1c1e9f48e36a3f7834c41b642936377c886
Dominic <dlehrmos@stud.hs-heilbronn.de>
2017-11-27 17:24:54 +0100

    added files

--- client/src/app/admin/menue/quizzes/admin-quiz.ts
+++ client/src/app/admin/menue/quizzes/admin-quiz.ts
@@ -1,4 +1,14 @@
 export class AdminQuiz {
+  constructor(public description: string,
+              public hint: string,
+              public name: string,
+              public _id: string,
+              public location: string,
+              public isActive: boolean) {
+  }
+}
+
+export class AdminQuizSingleAnswer extends AdminQuiz{
   constructor(public answer: string,
               public description: string,
               public hint: string,
@@ -6,5 +16,18 @@ export class AdminQuiz {
               public _id: string,
               public location: string,
               public isActive: boolean) {
+    super(description,hint,name,_id,location,isActive);
+  }
+}
+
+export class AdminQuizMultipleChoice extends AdminQuiz{
+  constructor(public answer: string[],
+              public description: string,
+              public hint: string,
+              public name: string,
+              public _id: string,
+              public location: string,
+              public isActive: boolean) {
+    super(description,hint,name,_id,location,isActive);
   }
 }
